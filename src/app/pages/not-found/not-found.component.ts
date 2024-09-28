91d1610704ef811423d6013f22c9336fd1024198
GiltMonster <lucassantoslukas2@gmail.com>
2024-09-27 21:13:31 -0300

    feat: pagina de notfound adicionada e personalizada

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
+  imports: [RouterModule],
+  standalone: true,
+})
+export class NotFoundComponent{
+
+  constructor() { }
+
+
+}
