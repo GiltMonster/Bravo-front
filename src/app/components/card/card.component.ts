e0468b528db554ec25b7b3ac4578fe32fce2e7f7
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-12 16:04:13 -0300

    Feat: adicionei o skeleton card para carregamento

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,21 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlatformService } from 'src/app/services/platform.service';
-import { IonCard, IonIcon, IonLabel, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonCardContent } from "@ionic/angular/standalone";
+import { IonCard, IonIcon, IonLabel, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonCardContent, IonSkeletonText } from "@ionic/angular/standalone";
 import Category from 'src/app/interfaces/Category';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { FormatPricePipe } from 'src/app/pipes/format-price.pipe';
 import { FormatPromoPipe } from 'src/app/pipes/format-promo.pipe';
+import { SkeletonCardComponent } from '../skeleton-card/skeleton-card.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  imports: [IonCardContent, IonButton, IonCardSubtitle, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonCard, FormatPricePipe, FormatPromoPipe],
+  imports: [IonSkeletonText, IonCardContent, IonButton, IonCardSubtitle, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonCard, FormatPricePipe, FormatPromoPipe, SkeletonCardComponent],
   standalone: true,
 })
 export class CardComponent implements OnInit {
 
   @Input() categories: Category[] = [];
+  @Input() isLoaded: boolean = false;
 
   isMobile: boolean;
 
