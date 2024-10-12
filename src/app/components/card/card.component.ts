94f1174bd36944492a38b59f77358953d446ada0
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-12 17:23:38 -0300

    Feat: arrumei a falta de de imagem pois algum animal apagou as imagens do banco

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,20 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlatformService } from 'src/app/services/platform.service';
-import { IonCard, IonIcon, IonLabel, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonCardContent, IonSkeletonText } from "@ionic/angular/standalone";
+import { IonCard, IonIcon, IonLabel, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonCardContent, IonSkeletonText, IonImg } from "@ionic/angular/standalone";
 import Category from 'src/app/interfaces/Category';
 import { Router } from '@angular/router';
 import { FormatPricePipe } from 'src/app/pipes/format-price.pipe';
 import { FormatPromoPipe } from 'src/app/pipes/format-promo.pipe';
 import { SkeletonCardComponent } from '../skeleton-card/skeleton-card.component';
+import { addIcons } from 'ionicons';
+import { carSportOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  imports: [IonSkeletonText, IonCardContent, IonButton, IonCardSubtitle, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonCard, FormatPricePipe, FormatPromoPipe, SkeletonCardComponent],
+  imports: [IonImg, IonSkeletonText, IonCardContent, IonButton, IonCardSubtitle, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonCard, FormatPricePipe, FormatPromoPipe, SkeletonCardComponent],
   standalone: true,
 })
-export class CardComponent implements OnInit {
+export class CardComponent{
 
   @Input() categories: Category[] = [];
   @Input() isLoaded: boolean = false;
@@ -25,12 +27,11 @@ export class CardComponent implements OnInit {
     platformService: PlatformService,
     private router: Router
   ) {
+    addIcons({carSportOutline});
     this.isMobile = platformService.isMobile();
-  }
-
-  ngOnInit(): void {
     console.log(this.categories);
   }
+
 
   goToDesc(id: number) {
     this.isMobile ? this.router.navigate(['/mobile/tabs/desc/produto/', id]) : this.router.navigate(['/desc/produto/', id]);
