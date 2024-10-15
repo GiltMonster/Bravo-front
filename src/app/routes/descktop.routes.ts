d27772b97e091f977393c0be7e56558a37a946d6
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-14 21:25:18 -0300

    Feat: declaração das rotas tas telas de usuário login, e criação de conta

--- src/app/routes/descktop.routes.ts
+++ src/app/routes/descktop.routes.ts
@@ -8,5 +8,13 @@ export const routes: Routes = [
   {
     path: 'desc/produto/:id',
     loadComponent: () => import('../pages/desc-produto/desc-produto.component').then((m) => m.DescProdutoComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('../pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../pages/create-account/create-account.page').then( m => m.CreateAccountPage)
   }
 ];
