1dfd6645692b2d5112790807fe9230715f8e2c08
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-14 21:15:48 -0300

    Feat: add routs para a navegação do header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { IonButton, IonHeader, IonToolbar } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonToolbar, IonHeader,  IonButton, RouterModule ],
+  imports: [IonToolbar, IonHeader,  IonButton ],
   standalone: true,
 })
 export class HeaderComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  home() {
+    this.router.navigate(['/']);
+  }
+  login() {
+    this.router.navigate(['/login']);
+  }
 
 }
