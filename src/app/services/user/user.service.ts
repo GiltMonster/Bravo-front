439cb4e4f02a598e8d55d2ed5da4dc61beb55411
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-14 21:27:53 -0300

    Feat: foi criado o service para manipulação de usuários

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createUser(user: User) {
+    return this.http.post<User>(`${environment.BASE_API_URL}createUsuario`, user);
+  }
+}
