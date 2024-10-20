577eec6d828e07a5db1a5a09fd469cafd113d04c
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-20 14:52:09 -0300

    Feat: adicionado o toast para feedback e agora faz a validação e verificação do email e cpf para que na criação não sejam duplicados.

--- src/app/pages/create-account/create-account.page.ts
+++ src/app/pages/create-account/create-account.page.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AbstractControl, FormControl, FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonInput, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonInput, IonIcon, IonCard, IonToast } from '@ionic/angular/standalone';
 import { PlatformService } from 'src/app/services/platform.service';
 import { addIcons } from 'ionicons';
-import { idCard, lockClosed, mail, people } from 'ionicons/icons';
+import { closeCircle, idCard, lockClosed, mail, people, save } from 'ionicons/icons';
 import { User } from 'src/app/interfaces/User';
 import { UserService } from 'src/app/services/user/user.service';
+import Message from 'src/app/interfaces/Message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.page.html',
   styleUrls: ['./create-account.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonInput, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
+  imports: [IonToast, IonCard, IonIcon, IonInput, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class CreateAccountPage {
 
   isMobile = this.platformService.isMobile();
+  msg: Message = { message: '', usuario: '' };
+  isToastOpen: boolean = false;
+  isError: boolean = false;
+  isDisable: boolean = true;
 
   protected createAccountForms = this.formBuilder.group({
     name: this.formBuilder.control('', { validators: [Validators.required] }),
@@ -30,9 +36,10 @@ export class CreateAccountPage {
   constructor(
     private platformService: PlatformService,
     private userService: UserService,
-    protected formBuilder: NonNullableFormBuilder
+    protected formBuilder: NonNullableFormBuilder,
+    protected router: Router
   ) {
-    addIcons({ lockClosed, mail, people, idCard });
+    addIcons({ lockClosed, mail, people, idCard, save, closeCircle });
   }
 
   passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
@@ -60,24 +67,36 @@ export class CreateAccountPage {
   }
 
   createAccount() {
+    this.isDisable = false;
+
     const user: User = {
       name: this.createAccountForms.get('name')!.value,
       cpf: this.removeMask(this.createAccountForms.get('cpf')!.value),
       email: this.createAccountForms.get('email')!.value.toLowerCase(),
       password: this.createAccountForms.get('password')!.value
-    }
-
-    console.log(user);
-
+    };
 
     this.userService.createUser(user).subscribe(
       (response) => {
-        console.log(response);
+        this.msg = response;
+        this.setOpen(true);
+        this.createAccountForms.reset();
+        this.isError = false;
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 3000);
+
       },
       (error) => {
-        console.log(error);
+        this.msg = error.error
+        this.isError = true;
+        this.setOpen(true);
+        this.isDisable = true;
       }
     );
+  }
 
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
   }
 }
