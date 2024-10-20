c695fb3003d20abfb56c9f00aa7647ecaf3ffaf9
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-20 01:09:49 -0300

    refactor:  tratando as diferenças dos temas claro e escuro

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput } from '@ionic/angular/standalone';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonCard, IonIcon } from '@ionic/angular/standalone';
 import { PlatformService } from 'src/app/services/platform.service';
+import { addIcons } from 'ionicons';
+import { lockClosed, mail } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonInput, IonButton, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule,]
+  imports: [IonIcon, IonCard, IonInput, IonButton, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule,]
 })
 export class LoginPage {
 
@@ -17,10 +19,10 @@ export class LoginPage {
 
   constructor(
     private platformService: PlatformService
-  ) { }
+  ) { addIcons({ lockClosed, mail }); }
 
   loginForms = new FormGroup({
-    email: new FormControl('',[ Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
 
