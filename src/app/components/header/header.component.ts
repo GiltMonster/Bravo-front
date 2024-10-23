bfa318f9e1d8301c89c955090818cf70ff2cb14d
GiltMonster <lucassantoslukas2@gmail.com>
2024-10-23 01:21:48 -0300

    Feat: header fazendo o processo de verificar se um usuário esta logado ou não

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonButton, IonHeader, IonToolbar } from "@ionic/angular/standalone";
+import { User } from 'src/app/interfaces/User';
+import { AuthLoginService } from 'src/app/services/auth/auth-login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonToolbar, IonHeader,  IonButton ],
+  imports: [IonToolbar, IonHeader, IonButton],
   standalone: true,
 })
 export class HeaderComponent {
 
+  userAuthenticated: User | boolean = false;
+
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private authService: AuthLoginService
+  ) {
+
+    this.authService.verifyToken().subscribe((user) => {
+      this.userAuthenticated = true;
+
+    }, (error) => {
+      console.log("Erro de autenticação", error);
+
+      this.userAuthenticated = false;
+    });
+  }
 
   home() {
     this.router.navigate(['/']);
   }
+
   login() {
     this.router.navigate(['/login']);
+  }
+
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+    this.router.navigate(['/']);
+  }
+
+  perfil() {
+    this.router.navigate(['/perfil']);
   }
 
 }
