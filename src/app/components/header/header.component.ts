b90a8b20e87da42e9a5bf8f29cf9b2497436081c
GiltMonster <lucassantoslukas2@gmail.com>
2024-11-16 20:43:06 -0300

    Feat: adicionei o badge(contador de itens do carrinho) ao header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonButton, IonHeader, IonToolbar } from "@ionic/angular/standalone";
+import { IonButton, IonHeader, IonToolbar, IonBadge, IonLabel } from "@ionic/angular/standalone";
 import { User } from 'src/app/interfaces/User';
 import { AuthLoginService } from 'src/app/services/auth/auth-login.service';
+import { CarrinhoService } from 'src/app/services/produto/carrinho.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonToolbar, IonHeader, IonButton],
+  imports: [IonLabel, IonBadge, IonToolbar, IonHeader, IonButton],
   standalone: true,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   userAuthenticated: User | boolean = false;
+  itensLength: number = 0;
 
   constructor(
     private router: Router,
-    private authService: AuthLoginService
+    private authService: AuthLoginService,
+    private carrinhoService: CarrinhoService
   ) {
+  }
 
+  ngOnInit(): void {
     this.authService.verifyToken().subscribe((user) => {
       this.userAuthenticated = true;
+
+      // Inscreve-se no BehaviorSubject para acompanhar o número de itens
+      this.carrinhoService.itensLength$.subscribe(length => {
+        this.itensLength = length;
+      });
+
+      // Carrega o carrinho inicialmente
+      this.carrinhoService.listaCarrinho(user.id!).subscribe((data) => {
+        this.itensLength = data.length;
+      });
 
     }, (error) => {
       console.log("Erro de autenticação", error);
@@ -36,6 +51,10 @@ export class HeaderComponent {
 
   login() {
     this.router.navigate(['/login']);
+  }
+
+  carrinho() {
+    this.router.navigate(['/carrinho']);
   }
 
   logout() {
