16b9f657637a7726cb8e74bbe5ceb6044b7dc891
GiltMonster <lucassantoslukas2@gmail.com>
2024-11-24 18:56:32 -0300

    Refact: se esta logado faz o tratamento de autentificação para validar quem é

--- src/app/components/tabs/tabs.page.ts
+++ src/app/components/tabs/tabs.page.ts
@@ -1,7 +1,9 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, inject, OnInit } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { bag, home, person } from 'ionicons/icons';
+import { User } from 'src/app/interfaces/User';
+import { AuthLoginService } from 'src/app/services/auth/auth-login.service';
 
 @Component({
   selector: 'app-tabs',
@@ -10,10 +12,26 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
-    addIcons({ triangle, ellipse, square });
+  authUser: User | boolean = false;
+
+  constructor(
+    private authService: AuthLoginService
+  ) {
+    addIcons({ home, bag, person });
   }
+
+  ngOnInit(): void {
+    this.authService.verifyToken().subscribe((user) => {
+      this.authUser = true;
+    },
+      (error) => {
+        console.log("Erro de autenticação", error);
+        this.authUser = false;
+      }
+    );
+  }
+
 }
