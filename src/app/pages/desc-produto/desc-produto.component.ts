355e05e06248025e54ba4b0640aa1a5fc8cd3a1a
GiltMonster <lucassantoslukas2@gmail.com>
2024-11-24 17:31:55 -0300

    Refact: foi adicionado a mensagem de feedback apos o usuário colocar um produto no carrinho

--- src/app/pages/desc-produto/desc-produto.component.ts
+++ src/app/pages/desc-produto/desc-produto.component.ts
@@ -1,21 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { IonContent, IonLabel, IonHeader, IonToolbar, IonTitle, IonSkeletonText, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { IonContent, IonLabel, IonHeader, IonToolbar, IonTitle, IonSkeletonText, IonButton, IonIcon, IonToast } from "@ionic/angular/standalone";
 import Product from 'src/app/interfaces/Product';
 import { FormatPricePipe } from 'src/app/pipes/format-price.pipe';
 import { ProdutoService } from 'src/app/services/home/produto.service';
 import { PlatformService } from 'src/app/services/platform.service';
 import { addIcons } from 'ionicons';
-import { basketOutline, cartOutline, cashOutline, carSportOutline } from 'ionicons/icons';
+import { basketOutline, cartOutline, cashOutline, carSportOutline, save, closeCircle, bagAdd } from 'ionicons/icons';
 import { CarrinhoService } from 'src/app/services/produto/carrinho.service';
 import { AuthLoginService } from 'src/app/services/auth/auth-login.service';
+import Message from 'src/app/interfaces/Message';
 
 @Component({
   selector: 'app-desc-produto',
   templateUrl: './desc-produto.component.html',
   styleUrls: ['./desc-produto.component.scss'],
-  imports: [IonIcon, IonButton, IonSkeletonText, IonTitle, IonToolbar, IonHeader, IonLabel, IonContent, CommonModule, RouterModule, FormatPricePipe],
+  imports: [IonToast, IonIcon, IonButton, IonSkeletonText, IonTitle, IonToolbar, IonHeader, IonLabel, IonContent, CommonModule, RouterModule, FormatPricePipe],
   standalone: true
 })
 export class DescProdutoComponent {
@@ -24,6 +25,11 @@ export class DescProdutoComponent {
   indexImagem: number = 0;
   isMobile: boolean = this.platformService.isMobile();
   isLoaded: boolean = false;
+
+  msg: Message = { message: '', usuario: '' };
+  isToastOpen: boolean = false;
+  isError: boolean = false;
+
 
   produto: Product = {
     id: 0,
@@ -50,9 +56,13 @@ export class DescProdutoComponent {
     private route: ActivatedRoute,
     private router: Router
   ) {
-    addIcons({ cartOutline, cashOutline, basketOutline, carSportOutline });
+    addIcons({ cartOutline, cashOutline, basketOutline, carSportOutline, closeCircle, bagAdd });
     this.getProductById();
 
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
   }
 
   getProductById() {
@@ -73,15 +83,21 @@ export class DescProdutoComponent {
       (data) => {
         this.carrinhoService.addItemCarrinho(data.id!, produto_id, quantidade).subscribe(
           (data) => {
-            console.log(data);
+            this.msg = data;
+            this.setOpen(true);
+            this.isError = false;
           },
           (error) => {
-            console.log(error);
+            this.msg = error.error
+            this.isError = true;
+            this.setOpen(true);
           }
         );
       },
       (error) => {
-        console.log(error);
+        this.msg = error.error
+        this.isError = true;
+        this.setOpen(true);
         this.router.navigate(['/login']);
       });
   }
