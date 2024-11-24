4fc56956a7057ae8033064e837beb99a90ee3f3c
GiltMonster <lucassantoslukas2@gmail.com>
2024-11-24 14:59:00 -0300

    Feat: utilizando o componente de footer na home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
-import { HeaderComponent } from 'src/app/components/header/header.component';
 import { PlatformService } from 'src/app/services/platform.service';
 import { PesquisaComponent } from "../../components/pesquisa/pesquisa.component";
 import { CardComponent } from "../../components/card/card.component";
 import Category from 'src/app/interfaces/Category';
 import { ProdutoService } from 'src/app/services/home/produto.service';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HeaderComponent, PesquisaComponent, CardComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PesquisaComponent, CardComponent, FooterComponent],
 })
 export class HomePage implements OnInit {
 
@@ -51,6 +50,5 @@ export class HomePage implements OnInit {
       });
     }
   }
-
 
 }
