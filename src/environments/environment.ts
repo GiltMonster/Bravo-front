56e3f391d36d14c9c8f7364cfeecfc72498360d9
GiltMonster <lucassantoslukas2@gmail.com>
2025-04-15 06:20:22 -0300

    Fix 🐞: Fix env config

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+const END_POINT = 'https://bravo-backend-production-b096.up.railway.app';
+
+export const environment = {
+  production: false,
+  BASE_API_URL: `${END_POINT}/api`,
+  API_URL_PRODUCTS: `${END_POINT}/api/produto`,
+  API_URL_USUARIO: `${END_POINT}/api/usuario`,
+  API_URL_CARRINHO: `${END_POINT}/api/carrinho`,
+  API_URL_ENDERECO: `${END_POINT}/api/endereco`,
+  API_URL_PEDIDO: `${END_POINT}/api/pedido`,
+  API_URL_AUTH_LOGIN: `${END_POINT}/api/auth/login`,
+  API_URL_AUTH_VERIFY: `${END_POINT}/api/auth/verify`
+};
